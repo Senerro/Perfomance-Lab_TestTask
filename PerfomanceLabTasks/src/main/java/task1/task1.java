425e36cd4bc66ec707b5f3656b7ee6d839e25510
Senerro <shadowpi1999@gmail.com>
2024-04-27 18:24:34 +0300

    added realization od custome circle collection

--- PerfomanceLabTasks/src/main/java/task1/task1.java
+++ PerfomanceLabTasks/src/main/java/task1/task1.java
@@ -1,51 +1,132 @@
 package task1;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 import static task1.taskHelpers.ArrayHelper.*;
 
 public class task1 {
-   /* class myCircularList {
-        Node first;
-        Node last;
-        LinkedList<Node> array;
+    static class myCircularList<T extends Number> {
+        private Node<T> first;
+        private Node<T> last;
+        private final ArrayList<ArrayList<Node<T>>> intervalsList = new ArrayList<>();
+        private int intervalSize;
 
-        class Node<T> {
-            Node next;
-            Node previous;
-            private T value;
+        private static class Node<T extends Number> {
+            Node<T> next;
+            Node<T> previous;
+            private final T value;
 
             public Node(T value) {
                 this.value = value;
             }
 
-            public T getValue() {
-                return value;
-            }
-
-            public void setValue(T value) {
-                this.value = value;
-            }
-
-            public Node getNext() {
-                return next;
-            }
-
-            public void setNext(Node next) {
+            public void setNext(Node<T> next) {
                 this.next = next;
             }
 
-            public Node getPrevious() {
-                return previous;
+            public void setPrevious(Node<T> previous) {
+                this.previous = previous;
             }
 
-            public void setPrevious(Node previos) {
-                this.previous = previos;
+            @Override
+            public String toString() {
+                return "Node{" + "value=" + value + '}';
             }
         }
 
-    }*/
+        public myCircularList() {
+            first = null;
+            last = null;
+        }
+
+        public myCircularList(int size, int interval) {
+            this();
+            myCircularListGenerator(size);
+            this.intervalSize = interval;
+            this.makeIntervals();
+        }
+
+        private void myCircularListGenerator(int size) {
+            for (int i = 0; i < size; i++)
+                add(i + 1);
+        }
+
+        public void add(int value) {
+            Node<T> node = new Node(value);
+            if (first == null) {
+                first = node;
+                last = node;
+            } else {
+                last.setNext(node);
+                node.setPrevious(last);
+                node.setNext(first);
+                first.setPrevious(node);
+                last = node;
+            }
+        }
+
+        public Node<T> iterate(Node<T> currentNode) {
+            return currentNode.next;
+        }
+
+        private void makeInterval(Node<T> start) {
+            intervalsList.add(new ArrayList<>());
+            Node<T> iterableNode = start;
+            for (int i = 0; i < intervalSize; i++) {
+                getLastIntervalOfList().add(iterableNode);
+                iterableNode = iterate(iterableNode);
+            }
+        }
+
+        public boolean checkLastNodeOfIntervalEqualsFirst() {
+            return getLast().equals(first);
+        }
+
+        private ArrayList<Node<T>> getLastIntervalOfList() {
+            return intervalsList.get(intervalsList.size() - 1);
+        }
+
+        private Node<T> getLast() {
+            return getLastIntervalOfList().get(getLastIntervalOfList().size() - 1);
+        }
+
+        private void makeIntervals() {
+            Node<T> currentNode = first;
+            do {
+                makeInterval(currentNode);
+                currentNode = getLast();
+            } while (!checkLastNodeOfIntervalEqualsFirst());
+        }
+
+        public void showIntervals() {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < intervalsList.size(); i++) {
+                stringBuilder.append("interval [").append(i + 1).append("] ");
+                for (var element : intervalsList.get(i))
+                    stringBuilder.append(element.value);
+                stringBuilder.append("\n");
+            }
+            System.out.println(stringBuilder);
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Route: ");
+            for (var list : intervalsList) {
+                builder.append(list.get(0).value);
+            }
+            return builder.toString();
+        }
+    }
     public static void main(String[] args) {
+        myCircularList<Integer> list = new myCircularList<>(5, 4);
+
+        list.showIntervals();
+        System.out.println(list);
+    }
+
+    private void arrayRealisation() {
         StringBuilder route = new StringBuilder();
         int pointer = 0, counter = 1;
         int[] circleArray = arrayInitialization();
@@ -53,7 +134,6 @@ public class task1 {
         do {
             System.out.print("Step [" + counter + "] ");
             for (int i = getIntervalSize(); i > 0; i--) {
-
                 System.out.print(circleArray[pointer]);
                 pointer = incrementPointer(pointer);
             }
@@ -73,5 +153,11 @@ public class task1 {
         System.out.println("Route is " + route);
     }
 
+    private void customCollectionRealisation() {
+        myCircularList<Integer> list = new myCircularList<>(5, 4);
+        list.makeIntervals();
+        list.showIntervals();
+        System.out.println(list);
+    }
 }
 
